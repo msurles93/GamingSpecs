50e6fa88d6c238deb3713264800410fe913e2054
msurles93 <m.surles93@gmail.com>
2015-07-26 09:04:14 -0400

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+            $("#division").click(function () {
+               $(this).replaceWith("<li>The Nintendo 64's central processing unit (CPU) is the NEC VR4300</li>");
+            });
+         });
+    $(document).ready(function() {
+            $("#ps1").click(function () {
+               $(this).replaceWith("<li>The Ps1 CPU: 32-bit RISC (33.9MHz)</li>");
+            });
+         });
+      $(document).ready(function() {
+            $("#xb360").click(function () {
+               $(this).replaceWith("<li>The xb360 128 VMX-128 registers per hardware thread</li>");
+            });
+         });
+        $(document).ready(function() {
+            $("#ps4").click(function () {
+               $(this).replaceWith("<li>Ethernet x1, IEEE 802.11b/g/n (2.4 GHz only), Bluetooth 2.1+EDR</li>");
+            });
+         });
